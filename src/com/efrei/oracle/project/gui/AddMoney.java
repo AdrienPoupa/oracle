bd1515876bb92dd87ea9d80305ed5856adc0d66b
fabien beaujean <fabienbeaudimi@hotmail.fr>
2017-03-30 19:16:45 +0200

    add other amount

--- src/com/efrei/oracle/project/gui/AddMoney.java
+++ src/com/efrei/oracle/project/gui/AddMoney.java
@@ -6,29 +6,20 @@ import javax.swing.*;
  * Created by Adrien on 29/03/2017.
  */
 public class AddMoney {
+
     private JPanel addMoneyJPanel;
     private JLabel addMoneyJLabel;
     private JLabel amountToAddJLabel;
-    private JFormattedTextField formattedTextField1;
+    private JLabel otherAmountJLabel;
+    private JComboBox amountToAddJComboBox;
+    private JFormattedTextField otherAmountJText;
     private JButton submitJButton;
 
-    public JPanel getAddMoneyJPanel() {
-        return addMoneyJPanel;
-    }
-
-    public JLabel getAddMoneyJLabel() {
-        return addMoneyJLabel;
-    }
-
-    public JLabel getAmountToAddJLabel() {
-        return amountToAddJLabel;
-    }
-
-    public JFormattedTextField getFormattedTextField1() {
-        return formattedTextField1;
-    }
-
-    public JButton getSubmitJButton() {
-        return submitJButton;
-    }
+    public JPanel getAddMoneyJPanel() { return addMoneyJPanel; }
+    public JLabel getAddMoneyJLabel() { return addMoneyJLabel; }
+    public JLabel getAmountToAddJLabel() { return amountToAddJLabel; }
+    public JLabel getOtherAmountJLabel() { return otherAmountJLabel; }
+    public JComboBox getAmountToAddJComboBox() { return amountToAddJComboBox; }
+    public JFormattedTextField getOtherAmountJText() { return otherAmountJText; }
+    public JButton getSubmitJButton() { return submitJButton; }
 }
